9efb5f73a6cdb6d995340453971df8611ba61a0c
Shovan Joshi <shovanj@gmail.com>
2016-04-15 17:24:55 -0500

    convert helper methods test class to spec style

--- test/helper_methods_test.rb
+++ test/helper_methods_test.rb
@@ -4,27 +4,12 @@ class A
   extend Asha::HelperMethods
 end
 
-class AshaHelperMethodsTest < Minitest::Test
+describe Asha::HelperMethods do
 
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
-  def setup
-    # Do nothing
-  end
-
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-
-  def teardown
-    # Do nothing
-  end
-
-  # Fake test
-  def test_hash_key
-    url = "www.example.com/rss"
-    hash_key = Digest::SHA1.hexdigest(url)
-    assert_equal "source:#{hash_key}", A.hash_key(url, :source)
+  it "should return hash_key" do
+    hash_key_using_class = A.hash_key("test", :source)
+    hash_key_computed = "source:#{Digest::SHA1.hexdigest 'test' }"
+    expect(hash_key_using_class).must_equal(hash_key_computed)
   end
 
 end
-
